398c353e9abcd24dfece6adce2314743081132ef
Prakhar Mishra <prakhar.mishra1990@gmail.com>
2020-02-25 17:25:50 +0530

    api-gateway: Initiated project

---
+++ nagarro-shop-api-gateway/src/test/java/com/nagarro/shop/nagarroshopapigateway/NagarroShopApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.shop.nagarroshopapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagarroShopApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
